869417ca898ca4ff4a2abae0561a1bdc4fb01509
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-07-26 22:43:53 +0200

    modify let and functions in script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-{function playGame(playerInput){
+{const playGame = function (playerInput){
 
   clearMessages();
 
-  function getMoveName(argMoveId){
+  const getMoveName = function (argMoveId){
     if(argMoveId == 1){
       return 'kamień';
     } else if(argMoveId == 2){
@@ -15,7 +15,7 @@
     return 'nieznany ruch';*/
   }
 
-  function displayResult(argComputerMove, argPlayerMove){
+  const displayResult = function (argComputerMove, argPlayerMove){
 	  if(argComputerMove == argPlayerMove){
 		  printMessage('Remis!');
     }
