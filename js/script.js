74980107358f5909f70eb7d4aec99de3a1b1416b
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-07-26 22:22:07 +0200

    change let to const in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function playGame(playerInput){
+{function playGame(playerInput){
 
   clearMessages();
 
@@ -31,11 +31,11 @@ function playGame(playerInput){
     }
 	}
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
 
 /*if(randomNumber == 1){
   computerMove = 'kamień';
@@ -50,7 +50,7 @@ function playGame(playerInput){
 
   console.log('Gracz wpisał: ' + playerInput);
 
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
 
 /*if(playerInput == '1'){
   playerMove = 'kamień';
@@ -77,3 +77,4 @@ document.getElementById('play-paper')
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(3);
 });
+}
